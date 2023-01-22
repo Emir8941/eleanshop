56484061a253a69b15ee88508a7bd6befbe88673
Emir <emirsagyndykov931@gmail.com>
2023-01-22 15:41:36 +0600

    sdfg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router';
+import Layout from './Layout/Layout';
+import Contact from './pages/ContactsPage/Contact';
+import Deliver from './pages/deliver/deliver';
+import Home from './pages/Home/Home';
+import Showroom from './pages/Showroom/Showroom';
+import './sass/style.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
+        <Route path='/Deliver' element={<Deliver/>}/>
+        <Route path='/ShowRoom' element={<Showroom/>}/>
+        <Route path='/Contacts' element={<Contact/>}/>
+      </Route>
+    </Routes>
+    </>
   );
 }
 
